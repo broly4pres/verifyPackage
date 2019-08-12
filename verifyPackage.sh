75c4f01004b2c680565e5017e650074ceb533715
root <root@box.attlocal.net>
2019-08-11 21:58:22 -0400

    first commit

---
+++ verifyPackage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+Architecture
+
+if [ $# -lt 1 ]
+then
+    echo """
+            You forgot an argument!
+            Example: ./verifyPackage.sh <package_name>
+            ./verify.sh ansible
+        """
+else
+    yum list $1 &> /dev/null
+    if [ $? -eq 0 ]; then
+        echo "YES, $1 INSTALLED!"
+    else
+        echo "NOPE!, $1 NOT INSTALLED!"
+    fi
+fi
